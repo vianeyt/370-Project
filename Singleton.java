91150df5ddf3d96ecdc62c9d7f0dc190799da1a4
chirag <chiragkumar2369@gmail.com>
2022-12-03 12:53:21 -0500

    Singleton

---
+++ Singleton.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Singleton {
+    private static Singleton instance = null;
+    private ArrayList<list> mList = new ArrayList<list>();
+    private ArrayList<Task> listOfTasks = new ArrayList<Task>();
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+
+    public void setList(ArrayList<list> mList){
+        this.mList = mList;
+    }
+
+    public void setTask(ArrayList<Task> listOfTasks){
+        this.listOfTasks = listOfTasks;
+    }
+
+    public ArrayList<list> getList(){
+        return this.mList;
+    }
+
+    public ArrayList<Task> getTask(){
+        return this.listOfTasks;
+    }
+
+    public void setMessage(String message){
+        System.out.println(message);
+    }
+}
